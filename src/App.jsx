e971997dc7a364482e7e4bf3aeddbaa7b0a96d31
Amit Mishra <aamitmishra364@gmail.com>
2025-09-04 20:31:32 +0530

    Login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Hello </h1>
+    <div className="App">
+      <Routes>
+      <Route path="/" element={<Login/>} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
